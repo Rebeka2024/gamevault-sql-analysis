626e5a008761eb75c65bba7d2923138babfb6a5d
Rebeka2024 <151570815+Rebeka2024@users.noreply.github.com>
2026-07-28 17:37:14 -0700

    Create sample_data.sql

---
+++ sample_data.sql
@@ -0,0 +1,34 @@
+USE GameVault;
+
+
+INSERT INTO Games VALUES
+(1,'Hogwarts Legacy','RPG','PC',2023),
+(2,'Cyberpunk 2077','RPG','PC',2020),
+(3,'Minecraft','Sandbox','Multiple',2011),
+(4,'Elden Ring','Action RPG','Console',2022),
+(5,'The Sims 4','Simulation','PC',2014);
+
+
+
+INSERT INTO Players VALUES
+(1,'DragonSlayer99','USA',24),
+(2,'PixelWizard','Canada',29),
+(3,'GameMaster','UK',21),
+(4,'NovaPlayer','USA',32);
+
+
+
+INSERT INTO Sales VALUES
+(1,1,24000000,144000000),
+(2,2,30000000,180000000),
+(3,3,300000000,900000000),
+(4,4,25000000,150000000),
+(5,5,20000000,400000000);
+
+
+
+INSERT INTO PlayerActivity VALUES
+(1,1,1,120),
+(2,2,3,500),
+(3,3,4,150),
+(4,4,5,300);
